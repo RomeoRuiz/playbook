8b849e4c7e5f041d76292e0f47619f34505b4c4e
Romeo Ruiz <romeo.ruiz.a@gmail.com>
2022-04-17 23:21:03 -0500

    Primeros 3 ejemplos de semana 2

---
+++ weekly_mission_2/examples/1.-Objetos/Ejemplo2.js
@@ -0,0 +1,7 @@
+// Ejemplo 2: Crear un objeto con propiedades
+const myObjetc2 = {
+    name: "Romeo",
+    age: 27
+  }
+  console.log("Ejemplo 2: Crear un objeto con propiedades")
+  console.log(myObjetc2)
